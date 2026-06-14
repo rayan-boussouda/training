596fe57f7f6fdb27b34986d08dd661d4509d9623
Rayan Boussouda <rayanboussouda@Rayans-MacBook-Air.local>
2026-06-14 10:26:43 +0100

    [KAN-5] update-movies

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -12,3 +12,19 @@ export const createMovie = async (
     next(error);
   }
 };
+
+export const updateMovie = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const movie = await movieService.updateMovie(
+      Number(req.params.id),
+      req.body,
+    );
+    res.status(200).json(movie);
+  } catch (error) {
+    next(error);
+  }
+};
